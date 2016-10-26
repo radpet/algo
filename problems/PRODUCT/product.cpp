56d388316b7000304a6ca291aa3f4a2ea09df7a5
Radoslav Petkov <radipirate@gmail.com>
2016-10-27 01:16:18 +0300

    add even faster solution

--- problems/PRODUCT/product.cpp
+++ problems/PRODUCT/product.cpp
@@ -12,7 +12,7 @@ int max(int a,int b){
 // O(2^n)
 int slow(){
   int result = a[0];
-  for(int subset=1;subset < (1<<n);subset++){
+  for(int subset=2;subset < (1<<n);subset++){
     int current = 1;
     for(int mask = 31;mask>=0;mask--){
       bool bit = subset & (1<<mask);
@@ -54,12 +54,54 @@ int fast(){
 }
 
 
+// O(n)
+int faster(){
+  int nc = 0;
+  int ln = 1;
+  int lni = -1;
+  for(int i=0;i<n;i++){
+    nc = nc +  (a[i]<0);
+    if(a[i]<0){
+      if(ln == 1){
+        ln = a[i];
+        lni = i;
+      }
+      if (ln<a[i]){
+        ln = a[i];
+        lni = i;
+      }
+    }
+  }
+  int sol = 1;
+
+  if(nc%2==0){
+    for(int i=0;i<n;i++){
+      sol*=a[i];
+    }
+  }else{
+    for(int i=0;i<n-1;i++){
+      if(i==lni){
+        continue;
+      }
+      sol*=a[i];
+    }
+    if(n-1 != lni){
+      sol*=a[n-1];
+    }
+
+  }
+
+  return sol;
+}
+
+
 int main(){
   cin>>n;
 
   for(int i=0;i<n;i++){
     cin>>a[i];
   }
-  //  cout<<slow()<<endl;
-  cout<<fast()<<endl;
+  //cout<<slow()<<endl;
+  //cout<<fast()<<endl;
+  cout<<faster()<<endl;
 }
