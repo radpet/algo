4f5a0496ca8e7f2a8e27eb7e6de29bf9a52d6db4
Radoslav Petkov <radipirate@gmail.com>
2017-11-17 17:51:02 +0200

    add hw4

---
+++ problems/MAXIMIZE/maximize.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (maximize list)
+ (define (abs x)
+   (if (< x 0)
+       (- x)
+       x
+       )
+   )
+  
+  (define (iter rest result x)
+           (if (null? rest)
+               result
+               (if (< (abs ((car rest) x) ) (abs result) )
+                   (iter (cdr rest) result x)
+                   (iter (cdr rest) ((car rest) x) x)
+               )
+            )
+    )
+  
+  (λ(x) (iter list ( (car list) x) x) )
+)
+
+((maximize (list (λ (x) (- x 10)) (λ (x) (- x 5))) ) 5)
+((maximize (list (λ (x) (- x 10)) (λ (x) (- x 5)))) 9)
