2f95bd4383e31bc3531949bd0d276759adf70235
Radoslav Petkov <radi@jboxers.com>
2019-03-24 12:37:03 +0200

    more sql

--- problems/EASY_SELECT_SQL/easy_select_sql.sql
+++ problems/EASY_SELECT_SQL/easy_select_sql.sql
@@ -3,3 +3,9 @@ select BIRTHDATE from movies.MOVIESTAR where name="Sandra Bullock";
 select name from movies.MOVIESTAR where name=(select starname from movies.STARSIN where movietitle=(select title from movies.MOVIE where title like '%Empire%' and year=1980));
 select name from movies.MOVIESTAR where gender="M" or address="Malibu";
 
+select model, speed, hd from pc.pc where price  <1200;
+select model, 1.1*price as p from pc.laptop order by p;
+select model, ram, screen from pc.laptop where price > 1000;
+select model from pc.printer where color="y";
+select model, speed, hd from pc.pc where (cd="12x" or cd="16x") and price<2000;
+select code, model, speed+ram+10*screen as r from pc.laptop order by r
